4b5a883fae8b98316f13b38153992d09171d2140
J-exe <113549279+J-exe@users.noreply.github.com>
2023-02-04 20:28:27 -0700

    updated test driver to call fill asm method

--- src/TST_RAST.C
+++ src/TST_RAST.C
@@ -9,6 +9,10 @@ int main()
     UINT8 *base = (UINT8 *)Physbase();
 	UINT32 *base_32 = (UINT32 *) Physbase();
 
+	_fill_screen(); /*asm fill screen method*/
+
+	Cconin();
+
 	_clear_screen(); /*asm clear screen method*/
 
 	Cconin();
