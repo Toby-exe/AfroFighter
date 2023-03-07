7fc2a2c873d1fa1f2ca98711ddaf76463f5c6525
Toby-exe <104709648+Toby-exe@users.noreply.github.com>
2023-03-07 15:15:04 -0700

    added animation and sprite related things

---
+++ src/ANIMS.C
@@ -0,0 +1,25 @@
+#include "anims.h"
+
+const UINT16 idelAnimationRS [] = {
+    bm_avatarIdleF1_RS,
+    bm_avatarIdleF2_RS,
+    bm_avatarIdleF3_RS,
+};
+
+const UINT16 idelAnimationLS [] = {
+    bm_avatarIdleF1_LS,
+    bm_avatarIdleF2_LS,
+    bm_avatarIdleF3_LS,
+};
+
+const UINT16 runAnimationRS [] = {
+    bm_avatarRunF1_RS,
+    bm_avatarRunF2_RS,
+    bm_avatarRunF3_RS,
+};
+
+const UINT16 runAnimationLS [] = {
+    bm_avatarRunF1_LS,
+    bm_avatarRunF2_LS,
+    bm_avatarRunF3_LS,
+};
---
+++ src/SPRITES.C
@@ -0,0 +1,3 @@
+#include "SPRITES.H"
+
+const UIN
