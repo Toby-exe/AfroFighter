c16ad3ce498e1e6c8afadefbf26735b22f33f647
J-exe <113549279+J-exe@users.noreply.github.com>
2023-03-12 21:04:13 -0600

    added a synchronous timer to game

--- src/GAME.C
+++ src/GAME.C
@@ -1,6 +1,7 @@
 #include "game.h"
 
 bool gameIsRunning = true;
+int CLEAR_TIMER = 0;
 
 void gameLoop() {
 
@@ -11,8 +12,11 @@ void gameLoop() {
 
     initPlayer(&model.player, 420, 112);
 
-    while (gameIsRunning != false) {
+    clear_screen(base);
 
+    while (gameIsRunning != false) {
+        processAsync(&model);
+        processSync(&model);
 
         /*profile pic box*/
         plotRect(base, 48, 48, 5, 5);
@@ -28,16 +32,38 @@ void gameLoop() {
 
         /*arena base/floor*/
         plotRectFill(base, 640, 48, 0, 354);
-
-        processAsync(&model);
     }
     return;
 }
 
+void processSync(Model *model) {
+
+    UINT8 *base = (UINT8 *) Physbase();
+    long old_ssp;
+    long *timer = 0x462;
+    UINT32 then, now, elapsed;
+
+    old_ssp = Super(0); /* enter privileged mode */
+    now = *timer;
+    Super(old_ssp);     /* exit privileged mode as soon as possible */
+
+    elapsed = now - then;
+    if (elapsed > 0)
+    {
+        /* trigger synchronous events based on timeElapsed */
+        clear_screen(base);
+        /* render model */
+        then = now;
+    } 
+}
+
 void processAsync(Model *model) {
-    unsigned int input;
+    UINT16 input;
     enum avatarEvents newEvent = Idle;
-    input = Cnecin();
+
+    if(Cconis()){
+        input = Cnecin();
+    }
 
     switch (input) {
         case q_KEY:
@@ -47,6 +73,7 @@ void processAsync(Model *model) {
         case a_KEY:
             newEvent = moveEv;
             break;
+
         default:
             break;
     }
